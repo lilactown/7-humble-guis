666e85e601ac7e78657b48acbaa68e39c1c35427
Will Acton <will@willacton.com>
2022-11-02 20:06:25 -0700

    GUI 4: Timer

---
+++ src/town/lilac/humble/app/gui_4.clj
@@ -0,0 +1,68 @@
+(ns town.lilac.humble.app.gui-4
+  (:require
+   [io.github.humbleui.app :as app]
+   [io.github.humbleui.paint :as paint]
+   [io.github.humbleui.ui :as ui]
+   [io.github.humbleui.window :as window]
+   [town.lilac.humble.app.state :as state]
+   [town.lilac.humble.progress :as progress]))
+
+(defn timer
+  [*config *timer on-reset]
+  (ui/dynamic
+   ctx
+   [scale (:scale ctx)]
+   (ui/default-theme
+    {:hui.progress/track-height         (* 10 scale)
+     :hui.progress/fill-track-active    (paint/fill 0xFF0080FF)
+     :hui.progress/fill-track-inactive  (paint/fill 0xFFD9D9D9)}
+    (ui/center
+     (ui/width
+      350
+      (ui/column
+       (ui/row (ui/label "Elapsed: ")
+               ;; progress bar is a little wider than it should be
+               (ui/gap 5 5)
+               (ui/dynamic
+                _ctx
+                [elapsed @*timer
+                 max (:value @*config)]
+                (progress/progress {:value elapsed
+                                    :max max}))
+               (ui/gap 5 5))
+       (ui/gap 10 10)
+       (ui/dynamic
+        _ctx
+        [elapsed @*timer]
+        (ui/label (str (float (/ elapsed 1000))
+                       "s")))
+       (ui/gap 10 10)
+       (ui/row
+        (ui/label "Duration:")
+        (ui/slider *config))
+       (ui/gap 20 20)
+       (ui/button on-reset (ui/label "Reset"))))))))
+
+
+(defn start!
+  []
+  (let [*config (atom {:max (* 60 1000 5) ; 5 min
+                       :value (* 60 1000) ; 1 min
+                       })
+        *timer (atom 0)
+        run-timer! (fn run-timer! []
+                     (future
+                       (Thread/sleep 100)
+                       (when (< @*timer (:value @*config))
+                         (swap! *timer + 100))
+                       (run-timer!)))
+        on-reset #(reset! *timer 0)]
+    (add-watch *timer ::redraw (fn [_ _ _ _] (state/redraw!)))
+    (reset! state/*app (timer *config *timer on-reset))
+    (run-timer!))
+  (state/redraw!)
+  (app/doui
+   (window/set-content-size @state/*window 800 400)))
+
+
+(start!)
