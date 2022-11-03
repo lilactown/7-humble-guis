0b109de5388fe9873e5c45b971953ddd20d1d64a
Will Acton <will@willacton.com>
2022-11-03 09:01:04 -0700

    GUI 5: Update & delete

--- src/town/lilac/humble/app/gui_5.clj
+++ src/town/lilac/humble/app/gui_5.clj
@@ -8,7 +8,7 @@
 
 
 (defn crud
-  [{:keys [*db *name *surname on-create]}]
+  [{:keys [*db *name *surname on-create on-select on-update on-delete]}]
   (ui/default-theme
    {}
    (ui/focus-controller
@@ -31,19 +31,27 @@
            (ui/vscroll
             (ui/dynamic
              _ctx
-             [db @*db]
+             [{:keys [entries selected]} @*db]
              (ui/column
-              (for [{:keys [name surname]} db
+              (for [[i {:keys [name surname]}] (map-indexed vector entries)
                     :let [label (ui/padding
                                  8
-                                 (ui/label (str surname ", " name))) ]]
+                                 (ui/label (str surname ", " name)))]]
                 (ui/hoverable
-                 (ui/dynamic
-                  ctx
-                  [hovered? (:hui/hovered? ctx)]
-                  (if hovered?
-                    (ui/rect (paint/fill 0xFFCFE8FC) label)
-                    label)))))))))))
+                 (ui/clickable
+                  {:on-click (fn [_] (on-select i))}
+                  (ui/dynamic
+                   ctx
+                   [hovered? (:hui/hovered? ctx)]
+                   (cond
+                     (= selected i)
+                     (ui/rect (paint/fill 0xFFAACCFF) label)
+
+                     hovered?
+                     (ui/rect (paint/fill 0xFFCFE8FC) label)
+
+                     :else
+                     label))))))))))))
        (ui/gap 5 5)
        (ui/column
         (ui/row
@@ -58,27 +66,46 @@
       (ui/row
        (ui/button on-create (ui/label "Create"))
        (ui/gap 10 10)
-       (ui/button nil (ui/label "Update"))
+       (ui/button on-update (ui/label "Update"))
        (ui/gap 10 10)
-       (ui/button nil (ui/label "Delete"))))))))
+       (ui/button on-delete (ui/label "Delete"))))))))
 
 
 (defn start!
   []
-  (let [*db (atom [{:name "Hans" :surname "Emil"}
-                   {:name "Max" :surname "Mustermann"}
-                   {:name "Roman" :surname "Tisch"}])
+  (let [*db (atom {:entries [{:name "Hans" :surname "Emil"}
+                             {:name "Max" :surname "Mustermann"}
+                             {:name "Roman" :surname "Tisch"}]
+                   :selected nil})
         *name (atom {:text ""})
         *surname (atom {:text ""})
+        on-select (fn [i]
+                    (swap! *db assoc :selected i)
+                    (swap! *name assoc :text (get-in @*db [:entries i :name]))
+                    (swap! *surname assoc :text (get-in @*db [:entries i :surname])))
         on-create (fn []
-                    (swap! *db conj {:name (:text @*name)
-                                     :surname (:text @*surname)})
+                    (swap! *db update :entries conj {:name (:text @*name)
+                                                     :surname (:text @*surname)})
+                    (swap! *db assoc :selected nil)
                     (swap! *name assoc :text "")
-                    (swap! *surname assoc :text ""))]
+                    (swap! *surname assoc :text ""))
+        on-update (fn []
+                    (let [i (:selected @*db)]
+                      (swap! *db assoc-in [:entries i] {:name (:text @*name)
+                                                        :surname (:text @*surname)})))
+        on-delete (fn []
+                    (let [i (:selected @*db)]
+                      (swap! *db update :entries
+                             #(into (subvec % 0 i)
+                                    (subvec % (inc i))))
+                      (swap! *db assoc :selected nil)))]
     (reset! state/*app (crud {:*db *db
                               :*name *name
                               :*surname *surname
-                              :on-create on-create})))
+                              :on-create on-create
+                              :on-select on-select
+                              :on-update on-update
+                              :on-delete on-delete})))
   (state/redraw!)
   (app/doui
    (window/set-content-size @state/*window 1000 500)))
