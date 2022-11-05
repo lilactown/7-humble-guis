2162815a8319ff13ec9d7e7ba8267575d6127f0b
Will Acton <will@willacton.com>
2022-11-04 22:08:55 -0700

    GUI 7: prettier UI

--- src/town/lilac/humble/app/gui_7.clj
+++ src/town/lilac/humble/app/gui_7.clj
@@ -1,6 +1,7 @@
 (ns town.lilac.humble.app.gui-7
   (:require
    [io.github.humbleui.app :as app]
+   [io.github.humbleui.paint :as paint]
    [io.github.humbleui.ui :as ui]
    [io.github.humbleui.window :as window]
    [town.lilac.humble.app.state :as state]
@@ -8,17 +9,34 @@
 
 (defn app
   []
-  (ui2/with-theme
+  (ui/default-theme
+   {:hui.text-field/border-radius 0
+    :hui.text-field/fill-bg-inactive (paint/fill 0xFFFFFFFF)}
+   (ui/focus-controller
     (ui/vscrollbar
      (ui/vscroll
       (ui/column
+       (ui/row
+        (ui/gap 26 20)
+        (for [j (map (comp str char) (range 65 91))]
+          (ui/width
+           80
+           (ui/center
+            (ui/label j)))))
        (for [i (range 100)]
          (ui/row
-          (for [j (range 26)]
-            (ui/padding
-             1
-             (ui/column
-              (ui/width 50 (ui/text-field (atom {})))))))))))))
+          (ui/width
+           25
+           (ui/valign
+            0.5
+            (ui/halign
+             0.3
+             (ui/label i))))
+          (for [j (map char (range 26))]
+            (ui/column
+             (ui/width
+              80
+              (ui/text-field (atom {})))))))))))))
 
 
 (defn start!
